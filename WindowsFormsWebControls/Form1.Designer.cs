6960ece8aae3cdbf68b52abb2dae79fe96afb7b8
MohammedAliIbrahim <mhalieg@yahoo.com>
2021-03-30 21:04:31 +0200

    .

--- WindowsFormsWebControls/Form1.Designer.cs
+++ WindowsFormsWebControls/Form1.Designer.cs
@@ -79,6 +79,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(947, 450);
             this.Controls.Add(this.webBrowser1);
             this.Controls.Add(this.button1);
